cb2e27d87746a51eb31a91f803a0932ac5a54c52
Behnam <b.ghavimi@tarent.de>
2020-09-09 11:52:54 +0200

    create LoadData Package

--- loadData/loadData.go
+++ loadData/loadData.go
@@ -8,10 +8,35 @@ import (
 	"net/http"
 )
 
+type LoadData struct {
+	UrlEntitieChanel chan urlEntitie
+	Conf             config
+}
+
+func New() LoadData {
+	var ld  LoadData
+	ld.UrlEntitieChanel, ld.Conf =loadData()
+	return ld
+}
+
 type urlEntitie struct {
 	Url   string
 	Path  string
 	State bool
+}
+
+func onlyValidUrlEntities(entities []urlEntitie) []urlEntitie {
+	var entitiesWithValidUrl []urlEntitie
+	for _, v := range entities {
+		if v.State == true {
+			entitiesWithValidUrl = append(entitiesWithValidUrl, v)
+		}
+	}
+	return entitiesWithValidUrl
+}
+
+type config struct {
+	MaxGoRoutine int
 }
 
 func tagValidUrl(urlEntities *[]urlEntitie) {
@@ -32,17 +57,7 @@ func tagValidUrl(urlEntities *[]urlEntitie) {
 	*urlEntities = cleanUrls
 }
 
-func onlyValidUrlEntities(entities []urlEntitie) []urlEntitie {
-	var entitiesWithValidUrl []urlEntitie
-	for _, v := range entities {
-		if v.State == true {
-			entitiesWithValidUrl = append(entitiesWithValidUrl, v)
-		}
-	}
-	return entitiesWithValidUrl
-}
-
-func loadData() []urlEntitie {
+func loadData() (chan urlEntitie, config) {
 	dat, err := ioutil.ReadFile("config/FilesToDownload.json")
 	var urlToDownload []urlEntitie
 	if err != nil {
@@ -53,7 +68,22 @@ func loadData() []urlEntitie {
 	log.Println("\n----------------", "\n", "Here is the list of corrupted urls:")
 	tagValidUrl(&urlToDownload)
 	log.Println("\n----------------", "\n", "Here is the list of urls to download:")
-	validUrltoDownload := onlyValidUrlEntities(urlToDownload)
-	fmt.Println(validUrltoDownload)
-	return validUrltoDownload
+	validUrlstoDownload := onlyValidUrlEntities(urlToDownload)
+	fmt.Println(validUrlstoDownload)
+	urlEntitieChanel := make(chan urlEntitie)
+	go func() {
+		for _, urlItem := range validUrlstoDownload {
+			urlEntitieChanel <- urlItem
+		}
+		close(urlEntitieChanel)
+	}()
+	var conf config
+	datConfig, errConfig := ioutil.ReadFile("config/maxGoRoutine.json")
+	if errConfig != nil {
+		log.Println(errConfig)
+	} else {
+		json.Unmarshal(datConfig, &conf)
+	}
+
+	return urlEntitieChanel, conf
 }
--- main/main.go
+++ main/main.go
@@ -2,66 +2,37 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
+	"goland/dlManagerV1/loadData"
 	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
-type urlEntitie struct {
-	Url   string
-	Path  string
-	State bool
-}
-
-func tagValidUrl(urlEntities *[]urlEntitie) {
-	var cleanUrls []urlEntitie
-	for _, urlItem := range *urlEntities {
-		resp, err := http.Head(urlItem.Url)
-		if err != nil {
-			urlItem.State = false
-			log.Println(err)
-		} else if resp.StatusCode != http.StatusOK {
-			urlItem.State = false
-			log.Println(err)
-		} else {
-			urlItem.State = true
-		}
-		cleanUrls = append(cleanUrls, urlItem)
-	}
-	*urlEntities = cleanUrls
-}
-
-func onlyValidUrlEntities(entities []urlEntitie) []urlEntitie {
-	var entitiesWithValidUrl []urlEntitie
-	for _, v := range entities {
-		if v.State == true {
-			entitiesWithValidUrl = append(entitiesWithValidUrl, v)
-		}
-	}
-	return entitiesWithValidUrl
-}
+var ld = loadData.New()
 
 type downloadWriter struct {
-	fileName string
-	total    uint64
-	size     uint64
+	fileName    string
+	total       uint64
+	size        uint64
+	printIndex  int64
+	startTimeDl time.Time
 }
 
 func (wc *downloadWriter) Write(p []byte) (int, error) {
 	n := len(p)
 	wc.total += uint64(n)
-	fmt.Printf("\rDownloading %s... %f %c ", wc.fileName, (float64(wc.total)/float64(wc.size))*100, '%')
+	elapsed := time.Since(wc.startTimeDl)
+	fmt.Printf("\rDownloading %s... %f %c  and elapsed time: %s", wc.fileName, (float64(wc.total)/float64(wc.size))*100, '%', elapsed)
 	return n, nil
 }
 
-func Download(url string, path string, wg *sync.WaitGroup) error {
+func Download(url string, path string, wg *sync.WaitGroup, printIndex int64) error {
 	out, err := os.Create(path)
 	if err != nil {
 		return err
@@ -73,66 +44,31 @@ func Download(url string, path string, wg *sync.WaitGroup) error {
 		return err
 	}
 	defer resp.Body.Close()
+	defer fmt.Println("")
 	size, err := strconv.Atoi(resp.Header.Get("Content-Length"))
-	start := time.Now()
-	counter := &downloadWriter{fileName: path, total: 0, size: uint64(size)}
+
+	counter := &downloadWriter{fileName: path, total: 0, size: uint64(size), printIndex: printIndex, startTimeDl: time.Now()}
 	if _, err = io.Copy(out, io.TeeReader(resp.Body, counter)); err != nil {
 		_ = out.Close()
 		return err
 	}
 
 	_ = out.Close()
-	elapsed := time.Since(start)
-	log.Printf("Download completed in %s", elapsed)
+
 	return nil
 }
 
-
-func loadData() (chan urlEntitie,config) {
-	dat, err := ioutil.ReadFile("config/FilesToDownload.json")
-	var urlToDownload []urlEntitie
-	if err != nil {
-		log.Println(err)
-	} else {
-		json.Unmarshal(dat, &urlToDownload)
-	}
-	log.Println("\n----------------", "\n", "Here is the list of corrupted urls:")
-	tagValidUrl(&urlToDownload)
-	log.Println("\n----------------", "\n", "Here is the list of urls to download:")
-	validUrlstoDownload := onlyValidUrlEntities(urlToDownload)
-	fmt.Println(validUrlstoDownload)
-	urlEntitieChanel := make(chan urlEntitie)
-	go func() {
-		for _, urlItem := range validUrlstoDownload {
-			urlEntitieChanel <- urlItem
-		}
-		close(urlEntitieChanel)
-	}()
-	var conf config
-	datConfig, errConfig := ioutil.ReadFile("config/maxGoRoutine.json")
-	if errConfig != nil {
-		log.Println(errConfig)
-	} else {
-		json.Unmarshal(datConfig, &conf)
-	}
-
-	return urlEntitieChanel, conf
-}
-
-type config struct{
-	MaxGoRoutine int
-}
-
-
 func main() {
-	urlEntitieChanel,conf := loadData()
+
+	urlEntitieChanel := ld.UrlEntitieChanel
+	conf := ld.Conf
 	log.Println("\n----------------", "\n", "Progress of downloads:")
 
 	maxGoRoutine := conf.MaxGoRoutine
 	var wg sync.WaitGroup
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-
+	var counter int64
 	for i := 0; i < maxGoRoutine; i++ {
 		wg.Add(1)
 		go func() {
@@ -145,8 +81,8 @@ func main() {
 					if !ok {
 						return
 					}
-					Download(valueFromChanel.Url, valueFromChanel.Path, &wg)
-					time.Sleep(time.Millisecond*2)
+					atomic.AddInt64(&counter, 1)
+					Download(valueFromChanel.Url, valueFromChanel.Path, &wg, atomic.LoadInt64(&counter))
 				}
 
 			}
@@ -155,4 +91,5 @@ func main() {
 	wg.Wait()
 
 	fmt.Println("Download Finished")
+
 }
